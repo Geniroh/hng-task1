985d29012f485094e445b7f166495252f0a64a4d
Geniroh <irochibuzor@gmail.com>
2022-11-01 12:49:58 +0100

    added ids

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
     <div className="container">
       <ProfileCard name="Irobuisi Chibuzor" image={IMG} />
       <div className="tree">
-        <LinkCard link="https://twitter.com/irobuisi" displayText="Geniroh" id="twitter"/>
+        <LinkCard link="https://twitter.com/irobuisi" displayText="@irobuisi"  id="twitter"/>
         <LinkCard link="https://training.zuri.team/" displayText="Zuri Team" id="btn_zuri" />
-        <LinkCard link="https://books.zuri.team/" displayText="Zuri Books" id="books" />
-        <LinkCard link="https://books.zuri.team/python-for-beginners?ref_id=irochibuzor" displayText="Python Books" id="book__python" />
-        <LinkCard link="https://background.zuri.team" displayText="Background Check For Coders" id="pitch" />
-        <LinkCard link="https://books.zuri.team/design-rules" displayText="Design Books" id="book__design" />
+        <LinkCard link="https://books.zuri.team/" displayText="Zuri Books" title="This is where you find books about design and coding" id="books" />
+        <LinkCard link="https://books.zuri.team/python-for-beginners?ref_id=irochibuzor" displayText="Python Books" title="Best place to fetch resources to jumpstart tech careers" id="book__python" />
+        <LinkCard link="https://background.zuri.team" displayText="Background Check For Coders" title="validate all your codes at the cheapest rate" id="pitch" />
+        <LinkCard link="https://books.zuri.team/design-rules" displayText="Design Books" title="Best place to get amazingly free designs" id="book__design" />
         <div className="flex justify-content-center attribution">
           <img src={SLACKIMG} alt="" />
           <img src={GITHUBIMG} alt="" />
--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const LinkCard = ({link, displayText}) => {
+export const LinkCard = ({link, displayText, title, id}) => {
   return (
     <div className="link-card" hidden>
-        <a href={link} target="_blank" rel="noreferrer"> {displayText} </a>
+        <a href={link} target="_blank" rel="noreferrer" title={title} id={id}> {displayText} </a>
     </div>
   );
 };
